0ebf481dbb8374b42cba340f1d61d9b80f5c3e4c
Thibault Rapin <thibault.rapin@insa-rennes.fr>
2014-10-08 23:12:14 +0200

    Adds Word and Cell classes

---
+++ Word.py
@@ -0,0 +1,11 @@
+# Object Word contain the string and its position(x,y)
+ 
+class Word:
+	def __init__(self, wordStr, posX, posY):
+		self.wordStr = wordStr
+		self.posX = posX
+		self.posY = posY
+	
+	#def __str__(self):
+	#	return self.str+" : ("+str(self.posX)+","+str(self.posY)+")\n"
+
