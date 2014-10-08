e402fb39f0e73f282ac9560ce6fce4727ee9499b
Thibault Rapin <thibault.rapin@insa-rennes.fr>
2014-10-08 20:35:22 +0200

    getting coordinates of rows and columns

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import re
 
 # PDF to extract
 input = sys.argv[1]
@@ -12,11 +13,21 @@ output = sys.argv[2]
 # Extract pdf into html
 os.system(("pdf2txt.py -t html %s > %s")%(input, output))
 i=0
+tabColumn = []
+tabRow = []
 for line in open(output):
 	# We search the points of the table which have a width and a height of 0px
 	if "width:0px; height:0px;" in line:
-  		i = i + 1
+		i = i + 1
 		# The 6 first point are used for the line of the table, they are useless
 		if i>6 and i!=42 and i!=48:
-			print line
-		print i
+			if i<12:
+				mColumn = re.search("left:(.*?)px",line)
+				tabColumn.append(int(mColumn.group(1)))
+			mRow = re.search("left:(.*?)px; top:(.*?)px;",line)
+			if(int(mRow.group(1)) == tabColumn[0]):
+				tabRow.append(int(mRow.group(2)))
+				
+print(tabColumn)
+print(tabRow)
+			
