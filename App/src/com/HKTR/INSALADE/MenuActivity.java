9bdc40edc218a2f882b76b79051acd38c4c5ee33
Hyukchan <hyukchan.k@gmail.com>
2014-10-24 01:01:22 +0200

    Changes graphical details in the menu activity

--- App/src/com/HKTR/INSALADE/MenuActivity.java
+++ App/src/com/HKTR/INSALADE/MenuActivity.java
@@ -2,6 +2,7 @@ package com.HKTR.INSALADE;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -22,10 +23,37 @@ public class MenuActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+
+        //Change header menu title font
+        Typeface fontPacifico = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        TextView headerMenuTitle = (TextView) findViewById(R.id.headerMenuTitle);
+        headerMenuTitle.setTypeface(fontPacifico);
+
         TextView date = (TextView) findViewById(R.id.dateText);
         TextView starterContent = (TextView) findViewById(R.id.starterContent);
         TextView mainCourseContent = (TextView) findViewById(R.id.mainCourseContent);
         TextView dessertContent = (TextView) findViewById(R.id.dessertContent);
+
+        TextView starterTitle = (TextView) findViewById(R.id.starterTitle);
+        TextView mainCourseTitle = (TextView) findViewById(R.id.mainCourseTitle);
+        TextView dessertTitle = (TextView) findViewById(R.id.dessertTitle);
+
+        //Change date font
+        Typeface fontExistenceLight = Typeface.createFromAsset(getAssets(), "fonts/Existence-Light.otf");
+        Typeface nexaRust = Typeface.createFromAsset(getAssets(), "fonts/NexaRustSlab-BlackShadow01.otf");
+        date.setTypeface(fontExistenceLight);
+
+        //Change menu content font
+        starterContent.setTypeface(fontExistenceLight);
+        mainCourseContent.setTypeface(fontExistenceLight);
+        dessertContent.setTypeface(fontExistenceLight);
+
+        //Change menu title font
+        starterTitle.setTypeface(nexaRust);
+        mainCourseTitle.setTypeface(nexaRust);
+        dessertTitle.setTypeface(nexaRust);
+
+
 
         DayModel currentDay = WeekModel.getWeekList().get(42).getWeek().get(WeekModel.getCurrentMenuId());// TODO : to generalize
         MenuModel currentMenu;
