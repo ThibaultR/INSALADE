375b786e586ed77cc0b8094316e74dd6860fbc36
Thibault Rapin <thibault.rapin@insa-rennes.fr>
2015-03-09 02:14:04 +0100

    Event activity -> now get information from server and display them

--- Insalade/src/com/HKTR/insalade/EventActivity.java
+++ Insalade/src/com/HKTR/insalade/EventActivity.java
@@ -4,20 +4,33 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Hyukchan Kwon (hyukchan.k@gmail.com)
  * @author Thibault Rapin (thibault.rapin@gmail.com)
  */
 public class EventActivity extends Activity {
+    JSONArray eventsArray;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         //Hide title bar
@@ -30,27 +43,82 @@ public class EventActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
+        //TODO if not internet acces look on memory
+        //Fetch event list and display them
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://37.59.123.110:443/events/";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET,
+                 url,
+                 null,
+                 new Response.Listener<JSONObject>() {
+                     @Override
+                     public void onResponse(JSONObject response) {
+                         Log.e("GET : ", response.toString());
+                         eventsArray = response.optJSONArray("events"); //TODO save last json for offline purpose
+                         if(eventsArray == null){
+                             Log.e("Array : ", "Pas d'event");
+                         } else {
+                             displayEventFragment(eventsArray);
+                         }
+                     }
+                 },
+                 new Response.ErrorListener() {
+                     @Override
+                     public void onErrorResponse(VolleyError error) {
+                         Log.e("GETError : ", "Marche pas");
+                     }
+                 }
+                )
+                {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("Authorization", "a37541a6-4b80-487d-88c2-c0b4bce927bf");
+
+                        return params;
+                    }
+                };
+
+        // Add the request to the RequestQueue.
+        queue.add(jsObjRequest);
+    }
+
+
+    public EventFragment createEventFragment(String eventTitle, String eventDescription, String eventImageUrl, String eventStartTime, String eventEndTime){
+        EventFragment fragment = new EventFragment();
+        Bundle args = new Bundle();
+        args.putString("Title", eventTitle);
+        args.putString("Description", eventDescription);
+        args.putString("ImageUrl", eventImageUrl);
+        String date = "Du " + eventStartTime + " au " + eventEndTime;
+        args.putString("Date", date);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void displayEventFragment(JSONArray jsonArray){
         LinearLayout eventContainer = (LinearLayout) findViewById(R.id.eventContainer);
         eventContainer.removeAllViews();
 
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        //TODO foreach event
-        fragmentTransaction.add(R.id.eventContainer, createEventFragment("Title de l'event", "description trop bien de l'event","tasoeur.png", null, null));//TODO date
+        for(int i = 0; i < jsonArray.length(); i++){
+            try {
+                JSONObject event = jsonArray.getJSONObject(i);
+                Log.e("event"+i+" : ", event.getString("title"));//TODO remove
+                fragmentTransaction.add(R.id.eventContainer, createEventFragment(event.getString("title"), event.getString("description"),event.getString("image_url"), event.getString("event_start"), event.getString("event_end")));
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
         fragmentTransaction.commit();
-    }
-
-
-    public EventFragment createEventFragment(String eventTitle, String eventDescription, String eventImageUrl, Date eventStartTime, Date eventEndTime){
-        EventFragment fragment = new EventFragment();
-        Bundle args = new Bundle();
-        args.putString("Title", eventTitle);
-        args.putString("Description", eventDescription);
-        args.putString("ImageUrl", eventImageUrl);
-        //TODO date + image ?
-        fragment.setArguments(args);
-        return fragment;
     }
 
     public void onClickPreviousButton(View view) {onBackPressed();}
--- Insalade/src/com/HKTR/insalade/EventFragment.java
+++ Insalade/src/com/HKTR/insalade/EventFragment.java
@@ -1,12 +1,20 @@
 package com.HKTR.insalade;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.io.InputStream;
 
 /**
  * @author Hyukchan Kwon (hyukchan.k@gmail.com)
@@ -19,7 +27,7 @@ public class EventFragment extends Fragment {
         String Title = args.getString("Title", "DefaultTitle");
         String Description = args.getString("Description", "DefaultDescription");
         String ImageUrl = args.getString("ImageUrl", "DefaultImageUrl");//TODO defaultimage
-        //TODO date
+        String Date = args.getString("Date", "Aujourd'hui maybe");
 
         // Inflate the layout for this fragment
         RelativeLayout eventFragment = (RelativeLayout) inflater.inflate(R.layout.event_fragment, container, false);
@@ -28,10 +36,36 @@ public class EventFragment extends Fragment {
         eventTitle.setText(Title);
         TextView eventDescription = (TextView) eventFragment.findViewById(R.id.eventDescription);
         eventDescription.setText(Description);
+        TextView eventDate = (TextView) eventFragment.findViewById(R.id.eventDate);
+        eventDate.setText(Date);
 
+        new DownloadImageTask((ImageButton) eventFragment.findViewById(R.id.eventImage)).execute("http://37.59.123.110/Web/web/uploads/documents/"+ImageUrl); //TODO save Image for offline purpose
 
         return eventFragment;
     }
 
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageButton bmImage;
 
+        public DownloadImageTask(ImageButton bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 }
