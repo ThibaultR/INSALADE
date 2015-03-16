083b9f99d82773a63ac8a64e26f83d8d0559adf1
Thibault Rapin <thibault.rapin@insa-rennes.fr>
2015-03-16 23:42:06 +0100

    Parameter activity -> repair design

--- Insalade/src/com/HKTR/insalade/ParametersActivity.java
+++ Insalade/src/com/HKTR/insalade/ParametersActivity.java
@@ -16,7 +16,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by Hyukchan on 12/03/2015.
+ * @author Hyukchan Kwon (hyukchan.k@gmail.com)
+ * @author Thibault Rapin (thibault.rapin@gmail.com)
  */
 public class ParametersActivity extends BaseActivity {
     ToggleButton notificationMenuInput;
